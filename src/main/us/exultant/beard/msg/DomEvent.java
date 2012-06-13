acde38f6ea0a6fb6d04d9d33ad4159281a2c06a3
hash <hash@exultant.us>
2012-06-13 01:23:00 -0700

    Add comment block showing an example click event coming in from jquery.

--- src/main/us/exultant/beard/msg/DomEvent.java
+++ src/main/us/exultant/beard/msg/DomEvent.java
@@ -44,4 +44,46 @@ public class DomEvent {
 	String $srcElementId;
 	//JSObject $srcFnPtr;	// secret.  used for demuxing.  unclear how to get this on js side.	// bs, this doesn't go in the event, it just shows up in the Ingress function args
 	// we could let you give a list of properties on the event source object that you want copied to you.  but i dunno what good could come of that.
+	
+	
+	
+	/*
+	 * An example click object:
+	 * 
+	originalEvent: [object MouseEvent]
+	type: click
+	timeStamp: 60622330
+	jQuery1720437917927164456: true
+	toElement: undefined
+	screenY: 736
+	screenX: 323
+	pageY: 650
+	pageX: 52
+	offsetY: undefined
+	offsetX: undefined
+	fromElement: undefined
+	clientY: 650
+	clientX: 52
+	buttons: undefined
+	button: 0
+	which: 1
+	view: [object Window]
+	target: [object HTMLDivElement]
+	shiftKey: false
+	relatedTarget: null
+	metaKey: false
+	eventPhase: 2
+	currentTarget: [object HTMLDivElement]
+	ctrlKey: false
+	cancelable: true
+	bubbles: true
+	altKey: false
+	srcElement: undefined
+	relatedNode: undefined
+	attrName: undefined
+	attrChange: undefined
+	delegateTarget: [object HTMLDivElement]
+	data: null
+	handleObj: [object Object]
+	*/
 }
